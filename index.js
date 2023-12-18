2f034a90846f507f90a1256618e306ea32ff36a5
julio cesar <jcmcf2012@gmail.com>
2023-12-18 20:02:23 -0300

    feat: add branch backup/cal

---
+++ index.js
@@ -0,0 +1,52 @@
+// function soma(num1 = 0, num2 = 0) {
+//   if (num1 < 0 && num2 < 0) return 0;
+
+//   return num1 + num2;
+// }
+
+// const resultado = soma();
+// const resultado2 = soma();
+// //console.log(resultado);
+
+// /*
+// for (inicialização; condição; incremento / decremento) {
+//   // Código a ser repetido
+// }
+// */
+// const users = [
+//   { name: "João", age: 25, city: "São Paulo" },
+//   { name: "Maria", age: 32, city: "Rio de Janeiro" },
+//   { name: "Pedro", age: 19, city: "Belo Horizonte" },
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(`Olá, amigo ${users[i].name}, sua idade é: ${users[i].age}`);
+// }
+
+// users.map((item, index) => {
+//   if (index === 10) console.log("Esse é o item 10", item);
+
+//   console.log("INDEX", index);
+//   console.log(`Olá, amigo ${item.name}, sua idade é: ${item.age}`);
+// });
+
+// Descobrir se é par ou impar
+
+// if (numero === 0) {
+//   console.log("NUMERO zero");
+// } else if (numero % 2 === 0) {
+//   console.log("PAR");
+// } else {
+//   console.log("IMPAR");
+// }
+// let numero = 3;
+// const cond = numero % 2 === 0;
+
+// switch (cond) {
+//   case true:
+//     console.log("PAR");
+//     break;
+//   case false:
+//     console.log("IMPAR");
+//     break;
+// }
